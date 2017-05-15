a51b1a1262a83867160678185d8ce8b65016857f
Hakon Schutt <hakon.schutt@drivtrafikk.no>
2017-05-15 15:14:56 +0200

    selected all elements, only styling left

--- index.php
+++ index.php
@@ -5,9 +5,19 @@
     echo '<div class="container">';
 
     require_once 'core/header.php';
+?>
+		<div class="container--inner">
+			<div class="container--inner--text">
+				<h1>Westerdals billigste øl</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac leo ut turpis vulputate porta sit amet vel elit. Sed nec risus in orci sollicitudin euismod ut eu ipsum.</p>
+			</div>
+	    </div>
+	    <img id="pilNed" src="Ressurser/Ikoner/Pilnedover.png">
+	</div>
+<?php
 
-    echo '</div>'; 
-    
 	require 'core/init.php';
     echo Init::footer();
+
 ?>
+
