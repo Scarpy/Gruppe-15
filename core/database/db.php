e6e1922be21b573e7f18fca4d2702dbebfc77217
ViciousCat <cmdeimos@hotmail.com>
2017-05-25 13:37:19 +0200

    ...

--- core/database/db.php
+++ core/database/db.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "tek.westerdals.no";
+$servername = "localhost";
 $port = "3306";
 $username = "limrui16_dbBeer";
 $password = "Billigsteol123";
@@ -14,12 +14,31 @@ if ($conn->connect_error) {
 $conn->set_charset("utf8");
 
 
+// pass på at norske tegn vises riktig
+$conn->set_charset("utf8");
+
+// raw sql
+$sql = "SELECT * FROM utesteder";
+// send inn query
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["navn"]. " - Avstand: " . $row["avstand"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+<p>ferdig xd <?= '' ?></p
 
 
 
 
 
-/*$host = "tek.westerdals.no";
+/*$host = "localhost";
     $db = "limrui16_billigsteol";
     $bruker = "limrui16_dbBeer";
     $pass = "Billigsteol123";
--- old/testest.php
+++ old/testest.php
@@ -1,5 +1,5 @@
 <?php 
 require '../core/database/db.php';
-echo "Connected successfully";
+;
 
 ?>
