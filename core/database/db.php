282b92aabc10525e732712b16def4f62778c067f
Hakon Schutt <hakon.schutt@drivtrafikk.no>
2017-05-25 22:22:34 +0200

    make db work with PDO

--- core/database/db.php
+++ core/database/db.php
@@ -1,5 +1,5 @@
 <?php
-<?php
+/*
 $servername = "tek.westerdals.no";
 $port = "3306";
 $username = "limrui16_dbBeer";
@@ -12,24 +12,21 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$conn->set_charset("utf8");
+$conn->set_charset("utf8");*/
 
-
-/*
-    $host = "localhost:3306";
-    $db = "limrui16_billigsteol";
-    $bruker = "limrui16_dbBeer";
-    $pass = "Billigsteol123";
+    $host = "localhost";
+    $db = "utesteder";
+    $bruker = "root";
+    $pass = "root";
 
     // Prøv å koble til databasen. Hvis problem oppstår, gi en feilmelding til bruker.
     try {
         $database = new PDO("mysql:host=$host;dbname=$db", "$bruker", "$pass");
         $database -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo 'work';
     } catch(PDOException $e) {
         echo "Beklager, det oppstod et problem med databasen. Gi beskjed til administrasjonen";
         exit();
     }
-*/
-?>
-?>
+
 
