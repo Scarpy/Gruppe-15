b11122d3b4634282b66250d857196d021ec919f0
Hakon Schutt <hakon.schutt@drivtrafikk.no>
2017-05-26 21:32:13 +0200

    style changes and added functionality to mat og rabatt

--- sokeside.php
+++ sokeside.php
@@ -3,8 +3,9 @@
         <h2 id="sokeOverskrift"> FINN DEN BILLIGSTE ØLEN </h2>
         <div class="sokContainer-wrapper">  
             <form action="result.php" id="sokemeny">
+                <span>Avstand i meter</span>
                 <input name="avstand" id="avstand" type="range" min="0" max="1000" value="0" step="100" onchange="visVerdi(this.value)" />
-                <span id="range">0</span>
+                <span id="range">0</span><span>m</span>
                 <script type="text/javascript">
                 function visVerdi(newValue)
                     {
@@ -13,8 +14,9 @@
                 </script>           
                     <br>
                     <br>
-               <input name="alder" id="alder" type="range" min="18" max="26" value="18" step="1" onchange="visAlder(this.value)" />
-                <span id="Alder">0</span>
+                <span>Aldersgrense</span>
+                <input name="alder" id="alder" type="range" min="18" max="26" value="18" step="1" onchange="visAlder(this.value)" />
+                <span id="Alder">0</span><span>år</span>
                 <script type="text/javascript">
                 function visAlder(newValue)
                     {
@@ -23,33 +25,40 @@
                 </script>
                     <br>
                     <br>
-               <input name="pris" id="pris" type="range" min="30" max="100" value="30" step="1" oninput="showValue(this.value)" />
-                <span id="alderTeller">0</span>
+                <span>Pris for øl</span>
+                <input name="pris" id="pris" type="range" min="30" max="100" value="30" step="1" oninput="showValue(this.value)" />
+                <span id="alderTeller">0</span><span>kr</span>
                 <script type="text/javascript">
                 function showValue(newValue)
                     {
                         document.getElementById("alderTeller").innerHTML=newValue;
                     }
                 </script>
-                    <br>
-                    <br>
-                  <input name="apning" id="apning" type="range" min="0" max="3" value="0" step="1" onchange="visAapning(this.value)" />
-                <span id="aapning">0</span>
-                <script type="text/javascript">
-                function visAapning(newValue)
-                    {
-                        document.getElementById("aapning").innerHTML=newValue;
-                    }
-                </script>
                 <br>
                 <br>
                 <div id="jaNei">
-                <span>Matservering </span> <input name="mat" id="mat" type="range" min="0" max="1" value="0" step="1" onchange="showValue(this.value)" />
-                <br>
-                <br>
-                <span>Studentrabatt</span> <input name="rabatt" id="rabatt" type="range" min="0" max="1" value="0" step="1" />  
-                <br>
-                <br>     
+                    <span>Matservering</span> 
+                    <input name="mat" id="mat" class="smallrange" type="range" min="0" max="1" value="0" step="1" onchange="visMatservering(this.value)" />
+                    <span id="matyn">0</span>
+                    <script type="text/javascript">
+                    function visMatservering(newValue)
+                        {
+                            document.getElementById("matyn").innerHTML=newValue;
+                        }
+                    </script>
+                    <br>
+                    <br>
+                    <span>Studentrabatt</span> 
+                    <input name="rabatt" id="rabatt" class="smallrange" type="range" min="0" max="1" value="0" step="1" onchange="visRabatt(this.value)" /> 
+                    <span id="rabattyn">0</span>
+                    <script type="text/javascript">
+                    function visRabatt(newValue)
+                        {
+                            document.getElementById("rabattyn").innerHTML=newValue;
+                        }
+                    </script> 
+                    <br>
+                    <br>     
                 </div>
                 <br>
                 <br>
